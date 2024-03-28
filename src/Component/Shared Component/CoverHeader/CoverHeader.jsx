962615be35a006caa57c48c883c42c949bc292fe
habibur-01 <h.r.habibur08@gmail.com>
2024-03-28 16:14:36 +0600

    add a hook as useMenu

--- src/Component/Shared Component/CoverHeader/CoverHeader.jsx
+++ src/Component/Shared Component/CoverHeader/CoverHeader.jsx
@@ -6,7 +6,7 @@ const CoverHeader = ({img , title, subTitle}) => {
     return (
         <div className="hero min-h-[700px]" style={{ backgroundImage: `url('${img}')` }}>
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
+            <div className="hero-content w-3/5 h-80 bg-[#15151576] backdrop-blur-sm text-center text-neutral-content">
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
                     <p className="mb-5 uppercase">{subTitle}</p>
--- src/Pages/Home/FeaturedItem/FeaturedItem.jsx
+++ src/Pages/Home/FeaturedItem/FeaturedItem.jsx
@@ -16,7 +16,7 @@ const FeaturedItem = () => {
                     <h3>March 20, 2023</h3>
                     <h3 className="uppercase">WHERE CAN I GET SOME?</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-                    <button className="btn btn-outline border-0 border-b-4 text-[#646Fcf]">Read More</button>
+                    <button className="btn btn-outline border-0 border-b-4 text-yellow-500">Read More</button>
                 </div>
 
             </div>
